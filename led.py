9642c4a8606fbb89d184c01fd9c726c3cc99dcb8
Chris Harrold <christophereharrold@hotmail.com>
2016-09-19 15:24:15 -0600

    minor pin update for consistency
    not using different pins means I can run both led and led2 at the same
    time

--- led.py
+++ led.py
@@ -4,7 +4,7 @@ import time
 #Define the pin we are going to use to power the LED
 # the number of times it will blink
 # and the time between blinks
-pina = 17
+pina = 6
 interations = 10
 interval = .50
 
