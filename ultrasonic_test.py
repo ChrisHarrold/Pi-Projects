6e3059f61b5a175f68d1985adf0265a509e00f06
Chris Harrold <christophereharrold@hotmail.com>
2017-12-09 14:27:40 -0700

    Dash

--- ultrasonic_test.py
+++ ultrasonic_test.py
@@ -54,8 +54,8 @@ if __name__ == '__main__':
         	GPIO.output(GPIO_TRIGGER, False)
         	time.sleep(2)
         	dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
+        	print ("Measured Distance = %.1f cm" % dist)
+        	time.sleep(1)
  
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
