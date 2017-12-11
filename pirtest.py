7f0a2821c08585bf31ebdcb1ca11b5f521a75a04
Chris Harrold <christophereharrold@hotmail.com>
2017-12-11 14:28:20 -0700

    different

--- pirtest.py
+++ pirtest.py
@@ -1,17 +1,21 @@
-
 import RPi.GPIO as GPIO
 import time
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(22, GPIO.IN)         #Read output from PIR motion sensor
-# GPIO.setup(3, GPIO.OUT)         #LED output pin
-while True:
-       i=GPIO.input(22)
-       if i==0:                 #When output from motion sensor is LOW
-             print "No intruders ",i
-             # GPIO.output(3, 0)  #Turn OFF LED
-             time.sleep(0.1)
-       elif i==1:               #When output from motion sensor is HIGH
-             print "Intruder detected ",i
-             # GPIO.output(3, 1)  #Turn ON LED
-             time.sleep(0.1)
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(23, GPIO.IN) #PIR
+GPIO.setup(24, GPIO.OUT) #BUzzer
+
+try:
+    time.sleep(2) # to stabilize sensor
+    while True:
+        if GPIO.input(23):
+            #GPIO.output(24, True)
+            #time.sleep(0.5) #Buzzer turns on for 0.5 sec
+            #GPIO.output(24, False)
+            print("Motion Detected...")
+            time.sleep(5) #to avoid multiple detection
+        time.sleep(0.1) #loop delay, should be less than detection delay
+
+except:
+    GPIO.cleanup()
