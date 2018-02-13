c8676a45b3153c18fdd36c5a0ca4f2b7bd30e930
Chris Harrold <christophereharrold@hotmail.com>
2018-02-13 10:34:27 -0700

    asdfasdffff

--- location.py
+++ location.py
@@ -18,24 +18,17 @@ headers = {'Content-type': 'application/json'}
 
 # grab our list of wifi networks and split them into the JSON data format
 # Scan using wlan0
-def cmdline(command):
-    process = Popen(
-        args=command,
-        stdout=PIPE,
-        shell=True
-    )
-    return process.communicate()[0]
+sLANs = cmdline('wpa_cli -i wlan0 scan_results')
+sLANs = sLANs.decode('utf-8')
 
 p = re.compile('''ur'(?:[0-9a-fA-F]:?){12}''')
 test_str = sLANs
-
 
 print(re.findall(p, test_str))
 
 
 X = '([a-fA-F0-9]{2}[:|\-]?){6}' # this is the regex
-sLANs = cmdline('wpa_cli -i wlan0 scan_results')
-sLANs = sLANs.decode('utf-8')
+
 for s in sLANs:
     a = re.compile(X).search(s)
     if a:
